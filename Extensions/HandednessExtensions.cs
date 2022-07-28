a404e3cb7fee3ea6353c2207d6fc09d5aaea12ba
TheUltiOne <ultioneofficial@gmail.com>
2022-07-27 20:49:00 -0400

    Add SharedLinkFromHandedness extension

--- Extensions/HandednessExtensions.cs
+++ Extensions/HandednessExtensions.cs
@@ -1,4 +1,6 @@
-﻿using OpenGloves_Unity.Data;
+﻿using System;
+using OpenGloves_Unity.Data;
+using OpenGloves_Unity.Shared;
 
 namespace OpenGloves_Unity.Extensions
 {
@@ -6,5 +8,18 @@ namespace OpenGloves_Unity.Extensions
     {
         public static string HandednessToString(this Handedness handedness)
             => handedness.ToString().ToLower();
+
+        public static SharedLink SharedLinkFromHandedness(this Handedness handedness)
+        {
+            switch (handedness)
+            {
+                case Handedness.Right:
+                    return SharedLinks.RightLink;
+                case Handedness.Left:
+                    return SharedLinks.LeftLink;
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(handedness), $"Unexpected handedness: {handedness}");
+        }
     }
 }
